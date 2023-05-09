0d5cbcae7ebe3d81efa3ae05f6b9cc57904b0772
Brian-jiangz <jiangz@cug.edu.cn>
2023-05-09 15:18:09 +0800

    myfuntion_py 2023/05/09

---
+++ data_rb.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+################################
+# This is a personal information
+# Establish time:2023-05-07
+# Modifications time:2023-05-07
+# Author:Jiang Zheng
+# Email:jiangzheng@qdio.ac.cn
+################################
+
+''' The purpose of this snippet is to '''
+
+
+import numpy as np
+
+
+def data_rb(filename):
+    '''
+    11
+    Args:
+       11
+    Returns:
+       11
+    '''
+    data = np.fromfile(filename, dtype=np.float32)
+    return data
+# In[ ]:
