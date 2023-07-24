155d0d7457316aab457bf6cf6e5e1a0a59fdcfcd
Brian-jiangz <jiangz@cug.edu.cn>
2023-07-24 14:14:19 +0800

    sss

---
+++ Copilot.py
@@ -0,0 +1,5 @@
+import settingsopilot
+
+'''
+Function to get the max and min values of a list
+'''
---
+++ Copilot_test.py
@@ -0,0 +1,9 @@
+'''
+write a code to print "holle world"
+'''
+
+# Your text to be output
+text = "Hello, this is a sample text!"
+
+# Output the text
+print(text)
